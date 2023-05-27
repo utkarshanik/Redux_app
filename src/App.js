377a787668b778210ce83a6a484d1de1936b8fd0
utkarshanik <utkarshanikam9@gmail.com>
2023-05-27 23:37:57 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Navbar from "./componant/Navbar";
+import Shop from "./componant/Shop";
+
+
+function App() {
+  return (
+    <div className="App">
+          <Navbar/>
+          <Shop/>
+          </div>
+  );
+}
+
+export default App;
---
+++ src/componant/Shop.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div className='container'>
+      
+          <h1 className='my-4'>Nick shoes Buy for 2000</h1>
+         <button className="btn btn-primary mx-2">-</button>
+          Add to cart
+         <button className="btn btn-primary mx-2">+</button>
+    </div>
+  )
+}
+
+export default Shop
